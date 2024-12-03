3ce543f47dd1d86f95374ebaa775a894f35fa3a5
Onur Güzelordu <148066014+onurguzelordu@users.noreply.github.com>
2024-12-03 12:19:53 +0300

    Gerekli düzeltmeler yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,11 @@ class HomeStyle extends StatelessWidget {
     "https://m.media-amazon.com/images/I/717yZN1f6bL._AC_UF1000,1000_QL80_.jpg",
     "https://sc04.alicdn.com/kf/Ha35b3662f2314e578ee628b92de127eeX.jpg",
     "https://m.media-amazon.com/images/I/81fmhkLY+kL.jpg",
-    "https://static.ticimax.cloud/cdn-cgi/image/width=540,quality=99/37144/uploads/urunresimleri/buyuk/6-katli-jut-dolap-ici-duzenleyici-21-x--b249-.jpg"
+    "https://ae01.alicdn.com/kf/S70b80a27839546d8b3f58fc50cd777f9j.jpg_640x640q90.jpg"
+    "https://mylamp.com.tr/UserFiles/Fotograflar/14404-gelincik-jut-dogal-hazeran-avize-50x33-cm-gelincik-hazeran-sarkit-lamba-8-png-gelincik-hazeran-sarkit-lamba-8.png",
+    "https://ideacdn.net/idea/kt/51/myassets/products/082/atlas-genc-odasi-14.jpg?revision=1731761783",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy8Ri8Mn0Ke2Qnyw5syP-cpzpeu48C5BC3NA&s",
+    "https://img.vivense.com/720x480/images/d62d19e6a9d94748802549a1afd7a90f.jpg",
   ];
 
   final List<String> productNames = const [
@@ -20,7 +24,11 @@ class HomeStyle extends StatelessWidget {
     "Tepsi Sehpa",
     "Palmiye Lambader",
     "Puf Koltuk",
-    "Dolap İçi Organizeri"
+    "Dolap İçi Organizeri",
+    "Avize",
+    "Atlas Üniteli Çalışma Masası",
+    "Motto Yeşil Dolap",
+    "Petek Raflar"
   ];
 
   final List<String> productDescriptions = const [
@@ -28,7 +36,11 @@ class HomeStyle extends StatelessWidget {
     "Sade görünümlü",
     "Egzotik bir atmosfer yaratan lambader",
     "Konforlu ve rahat kullanıma sahip",
-    "Uzun ömürlü kullanım ve pratik"
+    "Uzun ömürlü kullanım ve pratik",
+    "Doğadan gelen aydınlık...",
+    "Çalışma alanınızı ferhalatacak.",
+    "Şık tasarımı ve geniş depolama alanıyla evinizin havasını değiştirecek",
+    "Duvarlarınızı güzelleştirmenin en şık yolu"
   ];
 
   final List<double> productPrices = const [
@@ -37,138 +49,111 @@ class HomeStyle extends StatelessWidget {
     89.99,
     199.99,
     59.99,
+    40.00,
+    300.00,
+    320.00,
+    30.00,
   ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        drawer:  Drawer(
+        drawer: Drawer(
           backgroundColor: Colors.white,
-  child: Column(
-    children: [
-      DrawerHeader(
-        padding: EdgeInsets.all(60.0),
-        child: Text(
-          'Hoş Geldiniz',
-          style: TextStyle(
-            color: Colors.brown,
-            fontSize: 30
-
+          child: Column(
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.all(60.0),
+                child: Text(
+                  'Hoş Geldiniz',
+                  style: TextStyle(color: Colors.brown, fontSize: 30),
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero, // Varsayılan padding'i sıfırla
+                  children: [
+                    ListTile(
+                      leading: IconButton(
+                        icon: Icon(Icons.login), // Giriş simgesi
+                        onPressed: () {
+                          // Giriş yapma işlemleri burada yapılabilir
+                        },
+                      ),
+                      title: Text('Giriş Yapınız/Kayıt Olunuz'),
+                      textColor: Colors.black,
+                      onTap: () {
+                        // Giriş yapma veya kayıt olma işlemleri burada yapılabilir
+                      },
+                    ),
+                    ListTile(
+                      leading: IconButton(
+                        icon: Icon(Icons.store), // Mağaza simgesi
+                        onPressed: () {
+                          // Mağaza işlemleri burada yapılabilir
+                        },
+                      ),
+                      title: Text('Mağazalar'),
+                      textColor: Colors.black,
+                      onTap: () {
+                        // Mağazalar sayfasına yönlendirme burada yapılabilir
+                      },
+                    ),
+                    ListTile(
+                      leading: IconButton(
+                        icon: Icon(Icons.contact_mail), // İletişim simgesi
+                        onPressed: () {
+                          // İletişim işlemleri burada yapılabilir
+                        },
+                      ),
+                      title: Text('İletişim'),
+                      textColor: Colors.black,
+                      onTap: () {
+                        // İletişim sayfasına yönlendirme burada yapılabilir
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ListTile(
+                      title: Text('Masalar'),
+                      textColor: Colors.black,
+                      trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
+                      onTap: () {
+                        // İletişim sayfasına yönlendirme burada yapılabilir
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Lambaderler'),
+                      textColor: Colors.black,
+                      trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
+                      onTap: () {
+                        // İletişim sayfasına yönlendirme burada yapılabilir
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Sandalyeler'),
+                      textColor: Colors.black,
+                      trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
+                      onTap: () {
+                        // İletişim sayfasına yönlendirme burada yapılabilir
+                      },
+                    ),
+                    ListTile(
+                      title: Text('Koltuklar'),
+                      textColor: Colors.black,
+                      trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
+                      onTap: () {
+                        // İletişim sayfasına yönlendirme burada yapılabilir
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        
-      ),
-      Expanded(
-        child: ListView(
-          padding: EdgeInsets.zero, // Varsayılan padding'i sıfırla
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: Icon(Icons.login), // Giriş simgesi
-                onPressed: () {
-                  // Giriş yapma işlemleri burada yapılabilir
-                },
-              ),
-              title: Text('Giriş Yapınız/Kayıt Olunuz'),
-              textColor: Colors.black,
-              onTap: () {
-                // Giriş yapma veya kayıt olma işlemleri burada yapılabilir
-              },
-            ),
-            ListTile(
-              leading: IconButton(
-                icon: Icon(Icons.store),// Mağaza simgesi
-                onPressed: () {
-                  // Mağaza işlemleri burada yapılabilir
-                },
-              ),
-              title: Text('Mağazalar'),
-              textColor: Colors.black,
-              onTap: () {
-                // Mağazalar sayfasına yönlendirme burada yapılabilir
-              },
-            ),
-            ListTile(
-              leading: IconButton(
-                
-                icon: Icon(Icons.contact_mail), // İletişim simgesi
-                onPressed: () {
-                  // İletişim işlemleri burada yapılabilir
-                },
-              
-              ),
-              title: Text('İletişim'),
-              textColor: Colors.black,
-              onTap: () {
-                // İletişim sayfasına yönlendirme burada yapılabilir
-              },
-            ),
-
-SizedBox(height: 20,),
-
-ListTile(
-title: Text('Masalar'),
-textColor: Colors.black,
-              trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
-              onTap: () {
-                // İletişim sayfasına yönlendirme burada yapılabilir
-              },
-),
-
-ListTile(
-title: Text('Lambaderler'),
-textColor: Colors.black,
-              trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
-              onTap: () {
-                // İletişim sayfasına yönlendirme burada yapılabilir
-              },
-),
-
-ListTile(
-title: Text('Sandalyeler'),
-textColor: Colors.black,
-              trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
-              onTap: () {
-                // İletişim sayfasına yönlendirme burada yapılabilir
-              },
-),
-
-
-ListTile(
-title: Text('Koltuklar'),
-textColor: Colors.black,
-              trailing: Icon(Icons.arrow_forward), // Ok simgesi ekle
-              onTap: () {
-                // İletişim sayfasına yönlendirme burada yapılabilir
-              },
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          ],
-        ),
-      ),
-    ],
-  ),
-),
-
-
         appBar: AppBar(
           actions: [
             IconButton(onPressed: () {}, icon: Icon(Icons.search)),
@@ -256,6 +241,8 @@ textColor: Colors.black,
           ],
           currentIndex: 0,
           backgroundColor: Colors.brown,
+          selectedItemColor: Colors.white, // Seçili etiket rengi
+          unselectedItemColor: Colors.white, // Seçili olmayan etiket rengi
           onTap: (index) {
             // Implement button click logic here
           },
