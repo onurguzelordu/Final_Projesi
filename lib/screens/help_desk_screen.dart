a2589397baeeaa921e67a06fdd7f183c9ee3d67d
Onur Güzelordu <148066014+onurguzelordu@users.noreply.github.com>
2025-01-25 15:07:31 +0300

    Yardım ekranına geri gitme butonu eklendi.

--- lib/screens/help_desk_screen.dart
+++ lib/screens/help_desk_screen.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class HelpDeskScreen extends StatelessWidget {
   const HelpDeskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: 
+        Center(child: 
+           Text(
+              "Yardım Masası Ekranınız",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            
+            ),),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            context.go("/home");
+          },
+        ),
+      ),
       body: Center(
-        child: Text("Yardim Masasi Ekraniniz"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            
+            const SizedBox(height: 600),
+            OutlinedButton.icon(
+              onPressed: () {
+                context.go("/home");
+              },
+              style: OutlinedButton.styleFrom(
+   
+              ),
+              
+              label: const Text(
+                "Geri Dönünüz",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
 
+           Padding( padding: const EdgeInsets.only(top: 20.0),)
+          ],
+        ),
       ),
     );
   }
